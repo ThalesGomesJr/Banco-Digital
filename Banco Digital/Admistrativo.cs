346ffadcb59ecd52741e574178f5fcafae36a90e
Thales Junior <41759966+ThalesGomesJr@users.noreply.github.com>
2019-04-21 15:22:21 -0400

    Create Admistrativo.cs

---
+++ Banco Digital/Admistrativo.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlServerCe;
+
+namespace Banco_Digital
+{
+    public partial class Admistrativo : Form
+    {
+        public Admistrativo()
+        {
+            InitializeComponent();
+        }
+
+        private void btsair_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("Deseja sair de Administrativo?", "Sair", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.No) return;
+
+
+            else this.Close();
+            
+        }
+
+        private void btinserir_Click(object sender, EventArgs e)
+        {
+            Inserir inserir = new Inserir();
+            inserir.ShowDialog();
+        }
+
+        private void bteditar_Click(object sender, EventArgs e)
+        {
+            Visualizar_Excluir visualizar = new Visualizar_Excluir();
+            visualizar.ShowDialog();
+        }
+    }
+}
