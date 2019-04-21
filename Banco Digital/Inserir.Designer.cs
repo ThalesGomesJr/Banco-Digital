fb6701310c3bc14d68226befc96961e663088ab7
Thales Junior <41759966+ThalesGomesJr@users.noreply.github.com>
2019-04-21 15:24:41 -0400

    Create Inserir.Designer.cs

---
+++ Banco Digital/Inserir.Designer.cs
@@ -0,0 +1,248 @@
+﻿namespace Banco_Digital
+{
+    partial class Inserir
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Inserir));
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbnome = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.tbconta = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.tbnum_conta = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.tbcpf = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.tbtelefone = new System.Windows.Forms.TextBox();
+            this.btinserir = new System.Windows.Forms.Button();
+            this.btcancelar = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tbsenha = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.tbsaldo = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(172, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(116, 29);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "Cadastro";
+            // 
+            // tbnome
+            // 
+            this.tbnome.Location = new System.Drawing.Point(108, 84);
+            this.tbnome.Name = "tbnome";
+            this.tbnome.Size = new System.Drawing.Size(303, 20);
+            this.tbnome.TabIndex = 0;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(64, 87);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 13);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Nome:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(223, 170);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(76, 13);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "Tipo de conta:";
+            // 
+            // tbconta
+            // 
+            this.tbconta.Location = new System.Drawing.Point(302, 167);
+            this.tbconta.Name = "tbconta";
+            this.tbconta.Size = new System.Drawing.Size(108, 20);
+            this.tbconta.TabIndex = 4;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(28, 129);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(77, 13);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Numero conta:";
+            // 
+            // tbnum_conta
+            // 
+            this.tbnum_conta.Location = new System.Drawing.Point(108, 126);
+            this.tbnum_conta.Name = "tbnum_conta";
+            this.tbnum_conta.Size = new System.Drawing.Size(108, 20);
+            this.tbnum_conta.TabIndex = 1;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(72, 213);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(30, 13);
+            this.label6.TabIndex = 14;
+            this.label6.Text = "CPF:";
+            // 
+            // tbcpf
+            // 
+            this.tbcpf.Location = new System.Drawing.Point(107, 209);
+            this.tbcpf.Name = "tbcpf";
+            this.tbcpf.Size = new System.Drawing.Size(108, 20);
+            this.tbcpf.TabIndex = 5;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(248, 211);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(52, 13);
+            this.label8.TabIndex = 11;
+            this.label8.Text = "Telefone:";
+            // 
+            // tbtelefone
+            // 
+            this.tbtelefone.Location = new System.Drawing.Point(302, 208);
+            this.tbtelefone.Name = "tbtelefone";
+            this.tbtelefone.Size = new System.Drawing.Size(109, 20);
+            this.tbtelefone.TabIndex = 6;
+            // 
+            // btinserir
+            // 
+            this.btinserir.Location = new System.Drawing.Point(304, 259);
+            this.btinserir.Name = "btinserir";
+            this.btinserir.Size = new System.Drawing.Size(107, 27);
+            this.btinserir.TabIndex = 7;
+            this.btinserir.Text = "Inserir";
+            this.btinserir.UseVisualStyleBackColor = true;
+            this.btinserir.Click += new System.EventHandler(this.btinserir_Click);
+            // 
+            // btcancelar
+            // 
+            this.btcancelar.Location = new System.Drawing.Point(107, 259);
+            this.btcancelar.Name = "btcancelar";
+            this.btcancelar.Size = new System.Drawing.Size(107, 27);
+            this.btcancelar.TabIndex = 8;
+            this.btcancelar.Text = "Cancelar";
+            this.btcancelar.UseVisualStyleBackColor = true;
+            this.btcancelar.Click += new System.EventHandler(this.btcancelar_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(61, 170);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(41, 13);
+            this.label3.TabIndex = 16;
+            this.label3.Text = "Senha:";
+            // 
+            // tbsenha
+            // 
+            this.tbsenha.Location = new System.Drawing.Point(108, 166);
+            this.tbsenha.Name = "tbsenha";
+            this.tbsenha.Size = new System.Drawing.Size(108, 20);
+            this.tbsenha.TabIndex = 3;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(262, 129);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(37, 13);
+            this.label7.TabIndex = 18;
+            this.label7.Text = "Saldo:";
+            // 
+            // tbsaldo
+            // 
+            this.tbsaldo.Location = new System.Drawing.Point(302, 126);
+            this.tbsaldo.Name = "tbsaldo";
+            this.tbsaldo.Size = new System.Drawing.Size(108, 20);
+            this.tbsaldo.TabIndex = 2;
+            // 
+            // Inserir
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(463, 298);
+            this.ControlBox = false;
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.tbsaldo);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.tbsenha);
+            this.Controls.Add(this.btinserir);
+            this.Controls.Add(this.btcancelar);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.tbtelefone);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.tbcpf);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.tbnum_conta);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.tbconta);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.tbnome);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Inserir";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Inserir";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tbnome;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox tbconta;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox tbnum_conta;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox tbcpf;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox tbtelefone;
+        private System.Windows.Forms.Button btinserir;
+        private System.Windows.Forms.Button btcancelar;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox tbsenha;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox tbsaldo;
+    }
+}
