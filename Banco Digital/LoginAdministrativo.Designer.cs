b3f92ff09c33ab0a9b392a0edce65537a40c6bd3
Thales Junior <41759966+ThalesGomesJr@users.noreply.github.com>
2019-04-21 15:50:28 -0400

    Create LoginAdministrativo.Designer.cs

---
+++ Banco Digital/LoginAdministrativo.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace Banco_Digital
+{
+    partial class LoginAdministrativo
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbnome = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tbsenha = new System.Windows.Forms.TextBox();
+            this.btcancelar = new System.Windows.Forms.Button();
+            this.btentrar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // tbnome
+            // 
+            this.tbnome.Location = new System.Drawing.Point(69, 40);
+            this.tbnome.MaxLength = 100;
+            this.tbnome.Name = "tbnome";
+            this.tbnome.Size = new System.Drawing.Size(253, 20);
+            this.tbnome.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(24, 43);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Nome:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(24, 69);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Senha:";
+            // 
+            // tbsenha
+            // 
+            this.tbsenha.Location = new System.Drawing.Point(69, 66);
+            this.tbsenha.MaxLength = 100;
+            this.tbsenha.Name = "tbsenha";
+            this.tbsenha.PasswordChar = '*';
+            this.tbsenha.Size = new System.Drawing.Size(253, 20);
+            this.tbsenha.TabIndex = 1;
+            // 
+            // btcancelar
+            // 
+            this.btcancelar.Location = new System.Drawing.Point(69, 121);
+            this.btcancelar.Name = "btcancelar";
+            this.btcancelar.Size = new System.Drawing.Size(107, 27);
+            this.btcancelar.TabIndex = 3;
+            this.btcancelar.Text = "Cancelar";
+            this.btcancelar.UseVisualStyleBackColor = true;
+            this.btcancelar.Click += new System.EventHandler(this.btcancelar_Click);
+            // 
+            // btentrar
+            // 
+            this.btentrar.Location = new System.Drawing.Point(215, 121);
+            this.btentrar.Name = "btentrar";
+            this.btentrar.Size = new System.Drawing.Size(107, 27);
+            this.btentrar.TabIndex = 2;
+            this.btentrar.Text = "Entrar";
+            this.btentrar.UseVisualStyleBackColor = true;
+            this.btentrar.Click += new System.EventHandler(this.btentrar_Click);
+            // 
+            // LoginAdministrativo
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(370, 160);
+            this.Controls.Add(this.btentrar);
+            this.Controls.Add(this.btcancelar);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.tbsenha);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tbnome);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "LoginAdministrativo";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Login Administrativo";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbnome;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tbsenha;
+        private System.Windows.Forms.Button btcancelar;
+        private System.Windows.Forms.Button btentrar;
+    }
+}
